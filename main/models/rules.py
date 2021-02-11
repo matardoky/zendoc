184a32224bea08369f7970be78daf31c9666a07a
MatarDoky <mdokydiop@gmail.com>
2021-02-11 23:52:47 +0100

    Init module Rule

--- main/models/rules.py
+++ main/models/rules.py
@@ -18,6 +18,8 @@ from dateutil.rrule import (
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .authenticate import Company
+
 class Rule(models.Model):
 
     class Freqs(models.TextChoices):
@@ -36,4 +38,36 @@ class Rule(models.Model):
     )
     params= models.TextField(blank=True)
     _week_day= {"MO": MO, "TU": TU, "WE": WE, "TH": TH, "FR": FR, "SA": SA, "SU": SU}
+
+    company = models.ForeignKey(
+        Company, 
+        on_delete=models.CASCADE
+    )
+
+    def rrule_frequency(self):
+        compatibility_dict = {
+            "YEARLY": YEARLY,
+            "MONTHLY": MONTHLY,
+            "WEEKLY": WEEKLY, 
+            "DAILY": DAILY, 
+            "HOURLY": HOURLY,
+            "MINUTELY": MINUTELY,
+            "SECONDLY": SECONDLY,
+        }
+        return compatibility_dict[self.frequency]
+
+    def _weekday_or_number(self, param):
+        try:
+            return int(param)
+        except:
+            uparam = str(param).upper()
+            if uparam in Rule._week_day:
+                return Rule._week_day[uparam]
+    
+    def get_params(self):
+        pass
+
+
+    
+
 
