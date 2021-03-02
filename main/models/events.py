72a0d8c468fe8e3a8cf76aba94da1d0b144140fb
MatarDoky <mdokydiop@gmail.com>
2021-03-02 09:45:06 +0100

    les methodes de la classe Event

--- main/models/events.py
+++ main/models/events.py
@@ -33,89 +33,256 @@ class Event(models.Model):
     )
     motif= models.ForeignKey(Motif, on_delete=models.CASCADE)
 
-    def _create_occurrence(self, occ_start, occ_end=None):
-        """Create an Occurrence instance"""
-        if not occ_end:
-            occ_end = occ_start + (self.end - self.start)
-        return Occurrence(
-            event=self,
-            start=occ_start,
-            end=occ_end,
-            original_start=occ_start,
-            original_end=occ_end
-        )
+    def get_occurrences(self, start, end, clear_prefetch=True):
+       
+        if clear_prefetch:
+            self.occurrence_set._remove_prefetched_objects()
 
-    def _get_date_gen(self, rr, start, end):
-        """Return a generator to create the start dates for occurrences"""
-        date = rr.after(start)
-        while end and date <= end or not(end):
-            yield date
-            date = rr.after(date)
-    
-    def get_rrule_object(self):
-        if self.rule:
-            params = self.rule.get_params()
-            frequency = self.rule.rrule_frequency()
-            return rrule.rrule(frequency, dtstart= self.start, **params)
-    
-    def _get_occurrence_gen(self, start, end):
-        """Computes all occurrences for this event from start to end"""
-        length = self.end - self.start
-        if self.rule:
-            if self.end_recurring_period and end and (self.end_recurring_period < end):
-                end = self.end_recurring_period
-            occ_start_gen = self._get_date_gen(
-                self.get_rrule_object(),
-                start-length,
-                end
-            )
-            occ_start = next(occ_start_gen)
-            while not end or (end and occ_start <= end):
-                occ_end = occ_start + length
-                yield self._create_occurrence(occ_start, occ_end)
-                occ_start = next(occ_start_gen)
-        else:
-            if (not end or self.start < end) and self.end >= start:
-                occ_start_gen = self._get_date_gen(
-                    rrule.rrule(
-                        "DAILY",
-                        dtstart=self.start
-                    ),
-                    start - length,
-                    self.end
-                )
-                try:
-                    occ_start = next(occ_start_gen)
-                    while not end or (end and occ_start <= end):
-                        occ_end = occ_start + length
-                        yield self._create_occurrence(occ_start, occ_end)
-                        occ_start = next(occ_start_gen)
-                except StopIteration:
-                    pass
-        
-    def get_occurrences(self, start, end=None):
-        """Returns all occurrences from start to end."""
-        persistent_occurrences = self.occurrences.all()
-        occ_replacer = OccurrenceReplacer(persistent_occurrences)
-        occurrence_gen = self._get_occurrence_gen(start, end)
-        additional_occs = occ_replacer.get_additional_occurrences(start, end)
-        occ = next(occurrence_gen)
-        while not end or (occ.start < end or any(additional_occs)):
+        persisted_occurrences = self.occurrence_set.all()
+        occ_replacer = OccurrenceReplacer(persisted_occurrences)
+        occurrences = self._get_occurrence_list(start, end)
+        final_occurrences = []
+        for occ in occurrences:
+            # replace occurrences with their persisted counterparts
             if occ_replacer.has_occurrence(occ):
                 p_occ = occ_replacer.get_occurrence(occ)
-                if (end and p_occ.start < end) and p_occ.end >= start:
-                    estimated_occ = p_occ
+                # ...but only if they are within this period
+                if p_occ.start < end and p_occ.end >= start:
+                    final_occurrences.append(p_occ)
             else:
-                estimated_occ = occ
+                final_occurrences.append(occ)
+        # then add persisted occurrences which originated outside of this period but now
+        # fall within it
+        final_occurrences += occ_replacer.get_additional_occurrences(start, end)
+        return final_occurrences
 
-            if any(additional_occs) and (
-                    estimated_occ.start == additional_occs[0].start):
-                final_occ = additional_occs.pop(0)
+    def get_rrule_object(self, tzinfo):
+        if self.rule is None:
+            return
+        params = self._event_params()
+        frequency = self.rule.rrule_frequency()
+        if timezone.is_naive(self.start):
+            dtstart = self.start
+        else:
+            dtstart = tzinfo.normalize(self.start).replace(tzinfo=None)
+
+        if self.end_recurring_period is None:
+            until = None
+        elif timezone.is_naive(self.end_recurring_period):
+            until = self.end_recurring_period
+        else:
+            until = tzinfo.normalize(
+                self.end_recurring_period.astimezone(tzinfo)
+            ).replace(tzinfo=None)
+
+        return rrule.rrule(frequency, dtstart=dtstart, until=until, **params)
+
+    def _create_occurrence(self, start, end=None):
+        if end is None:
+            end = start + (self.end - self.start)
+        return Occurrence(
+            event=self, start=start, end=end, original_start=start, original_end=end
+        )
+
+    def get_occurrence(self, date):
+        use_naive = timezone.is_naive(date)
+        tzinfo = timezone.utc
+        if timezone.is_naive(date):
+            date = timezone.make_aware(date, timezone.utc)
+        if date.tzinfo:
+            tzinfo = date.tzinfo
+        rule = self.get_rrule_object(tzinfo)
+        if rule:
+            next_occurrence = rule.after(
+                tzinfo.normalize(date).replace(tzinfo=None), inc=True
+            )
+            next_occurrence = tzinfo.localize(next_occurrence)
+        else:
+            next_occurrence = self.start
+        if next_occurrence == date:
+            try:
+                return Occurrence.objects.get(event=self, original_start=date)
+            except Occurrence.DoesNotExist:
+                if use_naive:
+                    next_occurrence = timezone.make_naive(next_occurrence, tzinfo)
+                return self._create_occurrence(next_occurrence)
+
+    def _get_occurrence_list(self, start, end):
+      
+        if self.rule is not None:
+            duration = self.end - self.start
+            use_naive = timezone.is_naive(start)
+            tzinfo = timezone.utc
+            if start.tzinfo:
+                tzinfo = start.tzinfo
+
+            occurrences = []
+            if self.end_recurring_period and self.end_recurring_period < end:
+                end = self.end_recurring_period
+
+            start_rule = self.get_rrule_object(tzinfo)
+            start = start.replace(tzinfo=None)
+            if timezone.is_aware(end):
+                end = tzinfo.normalize(end).replace(tzinfo=None)
+
+            o_starts = []
+
+            closest_start = start_rule.before(start, inc=False)
+            if closest_start is not None and closest_start + duration > start:
+                o_starts.append(closest_start)
+
+            occs = start_rule.between(start, end, inc=True)
+            # The occurrence that start on the end of the timespan is potentially
+            # included above, lets remove if thats the case.
+            if len(occs) > 0:
+                if occs[-1] == end:
+                    occs.pop()
+            # Add the occurrences found inside timespan
+            o_starts.extend(occs)
+
+            # Create the Occurrence objects for the found start dates
+            for o_start in o_starts:
+                o_start = tzinfo.localize(o_start)
+                if use_naive:
+                    o_start = timezone.make_naive(o_start, tzinfo)
+                o_end = o_start + duration
+                occurrence = self._create_occurrence(o_start, o_end)
+                if occurrence not in occurrences:
+                    occurrences.append(occurrence)
+            return occurrences
+        else:
+            # check if event is in the period
+            if self.start < end and self.end > start:
+                return [self._create_occurrence(self.start)]
             else:
-                final_occ = estimated_occ
-            if not final_occ.cancelled:
-                yield final_occ
-            occ = next(occurrence_gen)
+                return []
+
+    def _occurrences_after_generator(self, after=None):
+   
+        tzinfo = timezone.utc
+        if after is None:
+            after = timezone.now()
+        elif not timezone.is_naive(after):
+            tzinfo = after.tzinfo
+        rule = self.get_rrule_object(tzinfo)
+        if rule is None:
+            if self.end > after:
+                yield self._create_occurrence(self.start, self.end)
+            return
+        date_iter = iter(rule)
+        difference = self.end - self.start
+        loop_counter = 0
+        for o_start in date_iter:
+            o_start = tzinfo.localize(o_start)
+            o_end = o_start + difference
+            if o_end > after:
+                yield self._create_occurrence(o_start, o_end)
+
+            loop_counter += 1
+
+    def occurrences_after(self, after=None, max_occurrences=None):
+     
+        if after is None:
+            after = timezone.now()
+        occ_replacer = OccurrenceReplacer(self.occurrence_set.all())
+        generator = self._occurrences_after_generator(after)
+        trickies = list(
+            self.occurrence_set.filter(
+                original_start__lte=after, start__gte=after
+            ).order_by("start")
+        )
+        for index, nxt in enumerate(generator):
+            if max_occurrences and index > max_occurrences - 1:
+                break
+            if len(trickies) > 0 and (nxt is None or nxt.start > trickies[0].start):
+                yield trickies.pop(0)
+            yield occ_replacer.get_occurrence(nxt)
+
+    @property
+    def event_start_params(self):
+        start = self.start
+        params = {
+            "byyearday": start.timetuple().tm_yday,
+            "bymonth": start.month,
+            "bymonthday": start.day,
+            "byweekno": start.isocalendar()[1],
+            "byweekday": start.weekday(),
+            "byhour": start.hour,
+            "byminute": start.minute,
+            "bysecond": start.second,
+        }
+        return params
+
+    @property
+    def event_rule_params(self):
+        return self.rule.get_params()
+
+    def _event_params(self):
+        freq_order = freq_dict_order[self.rule.frequency]
+        rule_params = self.event_rule_params
+        start_params = self.event_start_params
+        event_params = {}
+
+        if len(rule_params) == 0:
+            return event_params
+
+        for param in rule_params:
+            # start date influences rule params
+            if (
+                param in param_dict_order
+                and param_dict_order[param] > freq_order
+                and param in start_params
+            ):
+                sp = start_params[param]
+                if sp == rule_params[param] or (
+                    hasattr(rule_params[param], "__iter__") and sp in rule_params[param]
+                ):
+                    event_params[param] = [sp]
+                else:
+                    event_params[param] = rule_params[param]
+            else:
+                event_params[param] = rule_params[param]
+
+        return event_params
+
+    @property
+    def event_params(self):
+        event_params = self._event_params()
+        start = self.effective_start
+        empty = False
+        if not start:
+            empty = True
+        elif self.end_recurring_period and start > self.end_recurring_period:
+            empty = True
+        return event_params, empty
+
+    @property
+    def effective_start(self):
+        if self.pk and self.end_recurring_period:
+            occ_generator = self._occurrences_after_generator(self.start)
+            try:
+                return next(occ_generator).start
+            except StopIteration:
+                pass
+        elif self.pk:
+            return self.start
+        return None
+
+    @property
+    def effective_end(self):
+        if self.pk and self.end_recurring_period:
+            params, empty = self.event_params
+            if empty or not self.effective_start:
+                return None
+            elif self.end_recurring_period:
+                occ = None
+                occ_generator = self._occurrences_after_generator(self.start)
+                for occ in occ_generator:
+                    pass
+                return occ.end
+        elif self.pk:
+            return datetime.datetime.max
+        return None
 
 class Occurrence(models.Model):
 
--- main/models/utils.py
+++ main/models/utils.py
@@ -1,55 +1,170 @@
-import time
+import heapq
+from functools import wraps
 from django.utils import timezone
 
-def now(**kwargs):
-    return timezone.now(**kwargs).replace(second=0, microsecond=0)
-
-
-def monday_of_week(year, week):
+class EventListManager:
     """
-    Returns a datetime for the monday of the given week of the given year.
+    This class is responsible for doing functions on a list of events. It is
+    used to when one has a list of events and wants to access the occurrences
+    from these events in as a group
     """
-    str_time = time.strptime('{0} {1} 1'.format(year, week), '%Y %W %w')
-    date = timezone.datetime(
-        year=str_time.tm_year, month=str_time.tm_mon,
-        day=str_time.tm_mday, tzinfo=timezone.utc
+
+    def __init__(self, events):
+        self.events = events
+
+    def occurrences_after(self, after=None):
+        """
+        It is often useful to know what the next occurrence is given a list of
+        events.  This function produces a generator that yields the
+        the most recent occurrence after the date ``after`` from any of the
+        events in ``self.events``
+        """
+        from main.models import Occurrence
+
+        if after is None:
+            after = timezone.now()
+        occ_replacer = OccurrenceReplacer(
+            Occurrence.objects.filter(event__in=self.events)
         )
-    if timezone.datetime(year, 1, 4).isoweekday() > 4:
-        # ISO 8601 where week 1 is the first week that has at least 4 days in
-        # the current year
-        date -= timezone.timedelta(days=7)
-    return date
+        generators = [
+            event._occurrences_after_generator(after) for event in self.events
+        ]
+        occurrences = []
+
+        for generator in generators:
+            try:
+                heapq.heappush(occurrences, (next(generator), generator))
+            except StopIteration:
+                pass
+
+        while occurrences:
+            generator = occurrences[0][1]
+
+            try:
+                next_occurrence = heapq.heapreplace(
+                    occurrences, (next(generator), generator)
+                )[0]
+            except StopIteration:
+                next_occurrence = heapq.heappop(occurrences)[0]
+            yield occ_replacer.get_occurrence(next_occurrence)
 
 
-class OccurrenceReplacer(object):
+class OccurrenceReplacer:
     """
     When getting a list of occurrences, the last thing that needs to be done
     before passing it forward is to make sure all of the occurrences that
     have been stored in the datebase replace, in the list you are returning,
     the generated ones that are equivalent.  This class makes this easier.
     """
+
     def __init__(self, persisted_occurrences):
         lookup = [
-            ((occ.event, occ.original_start, occ.original_end), occ) for
-            occ in persisted_occurrences]
+            ((occ.event.id, occ.original_start, occ.original_end), occ)
+            for occ in persisted_occurrences
+        ]
         self.lookup = dict(lookup)
 
     def get_occurrence(self, occ):
         """
-        Return a persisted occurrences matching the occ and remove it from
-        lookup since it has already been matched
+        Return a persisted occurrences matching the occ and remove it from lookup since it
+        has already been matched
         """
         return self.lookup.pop(
-            (occ.event, occ.original_start, occ.original_end),
-            occ)
+            (occ.event.id, occ.original_start, occ.original_end), occ
+        )
 
     def has_occurrence(self, occ):
-        return (occ.event, occ.original_start, occ.original_end) in self.lookup
+        try:
+            return (occ.event.id, occ.original_start, occ.original_end) in self.lookup
+        except TypeError:
+            if not self.lookup:
+                return False
+            else:
+                raise TypeError(
+                    "A problem with checking if a persisted occurrence exists has occured!"
+                )
 
     def get_additional_occurrences(self, start, end):
         """
         Return persisted occurrences which are now in the period
         """
-        return [occ for key, occ in self.lookup.items() if (
-            (end and occ.start < end) and
-            occ.end >= start and not occ.cancelled)]
+        return [
+            occ
+            for _, occ in list(self.lookup.items())
+            if (occ.start < end and occ.end >= start and not occ.cancelled)
+        ]
+
+
+def get_kwarg_or_param(request, kwargs, key):
+    value = None
+    try:
+        value = kwargs[key]
+    except KeyError:
+        if request.method == "GET":
+            value = request.GET.get(key)
+        elif request.method == "POST":
+            value = request.POST.get(key)
+    return value
+
+
+def get_occurrence(request, **kwargs):
+    from main.models import Occurrence
+
+    occurrence_id = get_kwarg_or_param(request, kwargs, "occurrence_id")
+    return (
+        Occurrence.objects.filter(pk=occurrence_id).first() if occurrence_id else None
+    )
+
+
+def get_event(occurrence, request, **kwargs):
+    from main.models import Event
+
+    if occurrence:
+        event = occurrence.event
+    else:
+        event_id = get_kwarg_or_param(request, kwargs, "event_id")
+        event = Event.objects.filter(pk=event_id).first() if event_id else None
+    return event
+
+
+def get_calendar(event, request, **kwargs):
+    from main.models import Calendar
+
+    calendar = None
+    if event:
+        calendar = event.calendar
+    else:
+        calendar_slug = get_kwarg_or_param(request, kwargs, "calendar_slug")
+        calendar = (
+            Calendar.objects.filter(slug=calendar_slug).first()
+            if calendar_slug
+            else None
+        )
+    return calendar
+
+
+def get_objects(request, **kwargs):
+    occurrence = get_occurrence(request, **kwargs)
+    event = get_event(occurrence, request, **kwargs)
+    calendar = get_calendar(event, request, **kwargs)
+    return occurrence, event, calendar
+
+
+def coerce_date_dict(date_dict):
+    """
+    given a dictionary (presumed to be from request.GET) it returns a tuple
+    that represents a date. It will return from year down to seconds until one
+    is not found.  ie if year, month, and seconds are in the dictionary, only
+    year and month will be returned, the rest will be returned as min. If none
+    of the parts are found return an empty tuple.
+    """
+    keys = ["year", "month", "day", "hour", "minute", "second"]
+    ret_val = {"year": 1, "month": 1, "day": 1, "hour": 0, "minute": 0, "second": 0}
+    modified = False
+    for key in keys:
+        try:
+            ret_val[key] = int(date_dict[key])
+            modified = True
+        except KeyError:
+            break
+    return modified and ret_val or {}
