b6b2ceb2c8c8a4cabeb2bcdb24f9e23c8c465b6b
MatarDoky <mdokydiop@gmail.com>
2021-04-21 06:39:31 +0200

    Init Custom TuiCalendar

---
+++ src/components/Home/contains/CustomTuiCalendar.js
@@ -0,0 +1,12 @@
+import React, {
+    useRef,
+    useLayoutEffect,
+    useEffect,
+    forwardRef,
+    useImperativeHandle
+} from 'react'
+
+import TuiCalendar from "tui-calendar"
+import moment from 'moment'
+
+import "./style.less"
