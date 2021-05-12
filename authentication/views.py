3e654cb8c606269a5a3b1073b1fa57b669c56442
MatarDoky <mdokydiop@gmail.com>
2021-05-12 03:57:19 +0200

    RegistrationAPIView

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from datetime import datetime, date
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+
+from authentication.renderers import UserJSONRenderer
+from authentication.models import User
+from authentication.serializers import RegistrationSerializer
+from authentication.verification import SendEmail
+
+class RegistrationAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+    renderer_class = (UserJSONRenderer,)
+
+    def post(self, request):
+        user = request.data.get('user', {})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        SendEmail().send_verification_email(user.get('user', None), request)
+
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+
+
