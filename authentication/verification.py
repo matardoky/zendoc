adf864c43fbd60e9dd07f4f90dbddd288af64cda
MatarDoky <mdokydiop@gmail.com>
2021-05-12 03:43:52 +0200

    Verification email

---
+++ authentication/verification.py
@@ -0,0 +1,41 @@
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.utils import six
+from django.utils.encoding import force_text, force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from authentication.models import User
+from datetime import date
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type
+        )
+
+account_activation_token = TokenGenerator()
+
+
+class SendEmail():
+    def send_verification_email(self, email, request):
+        user = User.objects.filter(email=email).first()
+        subject = "Verify you Authors Haven account"
+
+        token = account_activation_token.make_token(user)
+        current_site = get_current_site(request)
+
+        body = render_to_string('mail.html', context={
+            'action_url':"http://",
+            'user':user,
+            'domain':current_site.domain,
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),
+            'token':token
+        })
+
+        mail = EmailMessage(subject, body, "matar@gmail.com", to=[email])
+        mail.content_subtype = 'html'
+        mail.send()
+
+        return (token, urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'))
